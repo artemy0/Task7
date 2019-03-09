5e5b3fef64869bf82ce9a3fb7bffb8d3122e8a46
Artem Gorbachev <gorbachev99artem@gmail.com>
2019-03-09 10:19:00 +0300

    change Crew name

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -6,58 +6,58 @@ namespace Task7
     {
         static void Main(string[] args)
         {
-            Crew workers = new Crew
+            Crew crew = new Crew
                 (
                 new Worker("Artem", "Gorbachev", 19, new Work(".NET Developer", "IBA", 0)),
                 new Worker("Alex", "Yakushkin", 19, new Work("enginer", "Integral", 600))
                 );
 
             //Add
-            if(!workers.IsReadOnly)
+            if(!crew.IsReadOnly)
             {
-                workers.Add(new Worker("Evgen", "Gorbachev", 25, new Work("marketer", "Mango", 600)));
-                workers.Insert(1, new Worker("Irea", "Shestakova", 21, new Work("HR", "Google", 800)));
+                crew.Add(new Worker("Evgen", "Gorbachev", 25, new Work("marketer", "Mango", 600)));
+                crew.Insert(1, new Worker("Irea", "Shestakova", 21, new Work("HR", "Google", 800)));
             }
 
 
             //Print
             Console.WriteLine("Source array!!!");
-            ShowInfo(workers);
+            ShowInfo(crew);
 
             //workers.Workers is array encapsulated in the class to be sorted and new CrewComparer() is rule by which the array will be sorted.
-            Array.Sort(workers.Workers, new CrewComparer());
+            Array.Sort(crew.Workers, new CrewComparer());
             Console.Write("\n\n\n");
 
             Console.WriteLine("Sorted array!!!");
-            ShowInfo(workers);
+            ShowInfo(crew);
 
             Console.Write("\n\n\n");
 
 
             //Remove
-            workers.RemoveAt(3);
-            workers.Remove(new Worker("Alex", "Yakushkin", 19, new Work("enginer", "Integral", 600)));
+            crew.RemoveAt(3);
+            crew.Remove(new Worker("Alex", "Yakushkin", 19, new Work("enginer", "Integral", 600)));
 
             //Add array
             Worker worker1 = new Worker("Vlad", "Yzlov", 37, new Work("policeman", "KGB", 1000));
             Worker worker2 = new Worker("Gleb", "Stepanov", 21, new Work("JS developer", "IBA", 1000));
-            workers.CopyTo(new Worker[] { worker1, worker2 }, 1);
+            crew.CopyTo(new Worker[] { worker1, worker2 }, 1);
 
 
             //Print result
-            Console.WriteLine($"Resulting array with count = {workers.Count}!!!");
-            ShowInfo(workers);
+            Console.WriteLine($"Resulting array with count = {crew.Count}!!!");
+            ShowInfo(crew);
 
 
-            workers.Clear();
+            crew.Clear();
 
             Console.ReadKey();
         }
 
         //Method for displaying information using foreach
-        public static void ShowInfo(Crew workers)
+        public static void ShowInfo(Crew crew)
         {
-            foreach (Worker worker in workers)
+            foreach (Worker worker in crew)
             {
                 worker.ShowWorkerInfo();
                 Console.WriteLine();
